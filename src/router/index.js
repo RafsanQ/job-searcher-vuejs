d19f4c7af8c984ea29e2bcffc3a7d85025ea5b8d
Rafsan <rafsan.quayes@gmail.com>
2023-06-16 16:47:16 +0600

    Created job details page and setup router link

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +20,13 @@ const router = createRouter({
       path: '/jobspage',
       name: 'jobspage',
       // route level code-splitting
-      component: () => import('../views/JobsPage.vue')
+      component: () => import('../views/Jobs/JobsPage.vue')
+    },
+    {
+      path: '/jobspage/:id',
+      name: 'jobsDetails',
+      // route level code-splitting
+      component: () => import('../views/Jobs/JobDetails.vue')
     }
   ]
 })
---
+++ src/views/Jobs/JobDetails.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Job Details Page</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
